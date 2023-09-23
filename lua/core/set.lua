657b6ead197754fbd97887c890a5fc5bcdf682f1
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2023-09-23 13:06:22 -0300

    fix: always use tab 4

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -1,21 +1,20 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-vim.opt.incsearch = true
-
+-- UI
 vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 5
-vim.opt.colorcolumn = "80"
-
-vim.opt.updatetime = 50
 vim.opt.showmode = false
+vim.opt.hlsearch = false
+
+-- Identation (always tab 4)
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = false
+vim.opt.autoindent = true
+
+-- Lines
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 5
+vim.opt.colorcolumn = "81"  -- Max length 80 chars
+
