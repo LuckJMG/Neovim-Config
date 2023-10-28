1c2778975ae06662cf57e2ac5dff41b44eec4ce3
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2023-10-28 10:37:36 -0300

    feat: add harpoon

--- lua/core/nav.lua
+++ lua/core/nav.lua
@@ -1,9 +1,21 @@
+-- tmux integration
+vim.cmd [[ noremap <silent> {ctrl-l} :<C-U>TmuxNavigateRight<cr> ]]
+
 -- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 
--- tmux integration
-vim.cmd [[ noremap <silent> {ctrl-l} :<C-U>TmuxNavigateRight<cr> ]]
+-- Harpoon
+local mark = require('harpoon.mark')
+local ui = require('harpoon.ui')
+
+vim.keymap.set('n', '<leader>m', mark.add_file)
+vim.keymap.set('n', '<leader>pm', ui.toggle_quick_menu)
+
+vim.keymap.set('n', '<A-h>', function() ui.nav_file(1) end)
+vim.keymap.set('n', '<A-j>', function() ui.nav_file(2) end)
+vim.keymap.set('n', '<A-k>', function() ui.nav_file(3) end)
+vim.keymap.set('n', '<A-l>', function() ui.nav_file(4) end)
 
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -56,6 +56,7 @@ require('lazy').setup({
 		'nvim-telescope/telescope.nvim', branch = '0.1.x',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
+	'ThePrimeagen/harpoon',
 
 	-- Code
 	{
