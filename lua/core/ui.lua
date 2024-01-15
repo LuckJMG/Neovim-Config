c5e529a2401b643abe98f791ef7b11e8b945db93
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2024-01-14 21:11:58 -0300

    feat: change diagnostics symbols to icons

--- lua/core/ui.lua
+++ lua/core/ui.lua
@@ -1,7 +1,15 @@
+-- Border to diagnostics
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 	opts = opts or {}
 	opts.border = opts.border or 'single'
 	return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+-- Set icons to diagnostics
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
