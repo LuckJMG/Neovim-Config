86f937a82cdabc486ebcc75cdc22c885b86c16c5
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2025-12-24 17:42:06 -0300

    fix: remove which-key plugin
    which-key plugin considered intrusive

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -47,17 +47,4 @@ return {
 		},
 		dependencies = { "MunifTanjim/nui.nvim" },
 	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	},
 }
